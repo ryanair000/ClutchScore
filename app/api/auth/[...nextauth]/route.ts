dc38915e7ec567775b9fe5ec5a2033193b0454b7
ryanair000 <nitradeufc@gmail.com@example.com>
2025-06-06 02:18:19 +0300

    Fix Netlify build error: Conditional assignment for session.user.id

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -44,7 +44,9 @@ const handler = NextAuth({
       return token;
     },
     async session({ session, token }) {
-      session?.user?.id = token?.id;
+      if (session.user) {
+        session.user.id = token.id;
+      }
       return session;
     },
   },
