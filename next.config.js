56ffe0ea02774b05f6653aee0077eb58155c7dcb
ryanair000 <nitradeufc@gmail.com@example.com>
2025-06-06 02:25:20 +0300

    Configure webpack to ignore optional ws dependencies and ensure interface file is clean

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = config.externals || {};
+      config.externals['bufferutil'] = 'bufferutil';
+      config.externals['utf-8-validate'] = 'utf-8-validate';
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
