9de54d1131fa0e1fd2b6ade5105908390f4b23bf
ryanair000 <nitradeufc@gmail.com@example.com>
2025-06-06 02:54:41 +0300

    Enable system theme in ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="light"
-            enableSystem={false}
+            enableSystem={true}
             disableTransitionOnChange
           >
             {children}
